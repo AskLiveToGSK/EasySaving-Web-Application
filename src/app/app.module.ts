61d35eedf34f5f94e224cb347c8484d7fd8f4c91
Gopal Singh Kumbhawat <askLive.gsk@gmail.com>
2020-06-03 10:31:42 +0530

    sdk added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+// API Sdk Services
+import { ApiModule } from './shared/sdk/api.module';
+import { ApiConfiguration } from './shared/sdk/api-configuration';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApiModule.forRoot(new ApiConfiguration ()),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
